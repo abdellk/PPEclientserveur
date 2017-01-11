8d18d700a1b207f7111bdee55189123cd9ee7a55
invite <invite@linux13.btssio>
2017-01-11 18:03:35 +0100

    Jounralisateur mongo

---
+++ 6-CreationUtilisateur/src/outils/Resultat.java
@@ -0,0 +1,40 @@
+package outils;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import modele.Utilisateur;
+import ressources.FournisseurDePersistance;
+
+public class Resultat {
+
+	private List<Utilisateur> employees;
+
+	public Resultat() {
+		super();
+	}
+
+	public void setEmployees(List<Utilisateur> employees) {
+		this.employees = employees;
+	}
+
+	public List<Utilisateur> getEmployees() {
+		return employees;
+	}
+	
+	public void enregistrer() {
+		EntityManager em = FournisseurDePersistance.getInstance().fournir();
+		em.getTransaction().begin();
+		System.out.println("TAILLE : " + employees.size());
+		for(Utilisateur obj : employees) {
+			// em.getTransaction().begin();
+			System.out.println(obj);
+			em.persist(obj);
+			// em.getTransaction().commit();
+		}
+		em.getTransaction().commit();
+		em.close();
+		FournisseurDePersistance.getInstance().detruire();
+	}
+}
